3beb3a1fb274a335f6d940280b8991f105ebcc05
kurrycat2004 <kurrycat2004@gmail.com>
2020-11-24 19:00:45 +0100

    comititiet

--- src/main/java/GameFrame.java
+++ src/main/java/GameFrame.java
@@ -9,85 +9,84 @@ import java.util.TimerTask;
 
 
 public class GameFrame extends JFrame {
-    public static JPanel panelObject;
-    public static PlayerFigure player1;
-    public static ArrayList<Bullet> bullets = new ArrayList<>();
-    public static GameFrame window;
-    public static BufferedImage gameOverImage;
-    public static BufferedImage tryAgainIcon;
-    public static BufferedImage exitIcon;
-    public static BufferedImage menuIcon;
-    public static BufferedImage menuBackground;
-    public static BufferedImage background;
+	public static JPanel panelObject;
+	public static PlayerFigure player1;
+	public static ArrayList<Bullet> bullets = new ArrayList<>();
+	public static GameFrame window;
+	public static BufferedImage gameOverImage;
+	public static BufferedImage tryAgainIcon;
+	public static BufferedImage exitIcon;
+	public static BufferedImage menuIcon;
+	public static BufferedImage menuBackground;
+	public static BufferedImage background;
 
-    static {
-        try {
-            gameOverImage = ImageIO.read(GameFrame.class.getResource("game_over.png"));
-            tryAgainIcon = ImageIO.read(GameFrame.class.getResource("restart_button.png"));
-            menuIcon = ImageIO.read(GameFrame.class.getResource("menu_button.png"));
-            background = ImageIO.read(GameFrame.class.getResource("game.background.png"));
-            exitIcon = ImageIO.read(GameFrame.class.getResource("exit_button.png"));
+	static {
+		try {
+			gameOverImage = ImageIO.read(GameFrame.class.getResource("game_over.png"));
+			tryAgainIcon = ImageIO.read(GameFrame.class.getResource("restart_button.png"));
+			menuIcon = ImageIO.read(GameFrame.class.getResource("menu_button.png"));
+			background = ImageIO.read(GameFrame.class.getResource("game.background.png"));
+			exitIcon = ImageIO.read(GameFrame.class.getResource("exit_button.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		window = new GameFrame();
+		window.setName("Dodge Game");
+		window.setTitle("Dodge Game");
+		//window.getFullscreen();
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
 
-    public static void main(String[] args) {
-        Music sound = new Music();
-        window = new GameFrame();
-        window.setName("Dodge Game");
-        window.getFullscreen();
-        window.setVisible(true);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panelObject = new Draw();
+		panelObject.setBackground(Color.DARK_GRAY);
+		panelObject.addKeyListener(new KeyHandler());
+		panelObject.addMouseListener(new MouseHandler());
+		window.add(panelObject);
 
-        panelObject = new Draw();
-        panelObject.setBackground(Color.DARK_GRAY);
-        panelObject.addKeyListener(new KeyHandler());
-        panelObject.addMouseListener(new MouseHandler());
-        window.add(panelObject);
+		player1 = new PlayerFigure();
 
-        player1 = new PlayerFigure();
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new Repaint(), 0, 1000 / 60);
+		timer.scheduleAtFixedRate(new Update(), 0, 1000 / 60);
+		timer.scheduleAtFixedRate(new ScoreUpdate(), 0, 100);
+	}
 
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new Repaint(), 0, 1000 / 100);
-        timer.scheduleAtFixedRate(new Update(), 0, 1000 / 200);
-        timer.scheduleAtFixedRate(new ScoreUpdate(), 0, 100);
-        sound.start();
-    }
+	public static class Repaint extends TimerTask {
+		@Override
+		public void run() {
+			if (GameFrame.player1.alive) {
+				double x;
+				double y;
+				if (Math.random() < 0.5) {
+					y = (int) (Math.random() * GameFrame.panelObject.getHeight());
+					if (Math.random() < 0.5) x = 0;
+					else x = GameFrame.panelObject.getWidth();
+				} else {
+					x = (int) (Math.random() * GameFrame.panelObject.getWidth());
+					if (Math.random() < 0.5) y = 0;
+					else y = GameFrame.panelObject.getHeight();
+				}
 
-    public static class Repaint extends TimerTask {
-        @Override
-        public void run() {
-            if (GameFrame.player1.alive) {
-                double x;
-                double y;
-                if (Math.random() < 0.5) {
-                    y = (int) (Math.random() * GameFrame.panelObject.getHeight());
-                    if (Math.random() < 0.5) x = 0;
-                    else x = GameFrame.panelObject.getWidth();
-                } else {
-                    x = (int) (Math.random() * GameFrame.panelObject.getWidth());
-                    if (Math.random() < 0.5) y = 0;
-                    else y = GameFrame.panelObject.getHeight();
-                }
+				double offX = player1.x - x;
+				double offY = player1.y - y;
+				double dist = Math.sqrt(offX * offX + offY * offY);
 
-                double offX = player1.x - x;
-                double offY = player1.y - y;
-                double dist = Math.sqrt(offX * offX + offY * offY);
+				int noise = 100;
+				offX += Math.random() * noise - noise / 2d;
+				offY += Math.random() * noise - noise / 2d;
 
-                int noise = 100;
-                offX += Math.random() * noise - noise / 2d;
-                offY += Math.random() * noise - noise / 2d;
-
-                offX /= dist;
-                offY /= dist;
-                offX *= 2;
-                offY *= 2;
-                if (Math.random() < 0.05 + Draw.score / 5000d)
-                    GameFrame.bullets.add(new Bullet(x, y, offX, offY));
-            }
-            //}
+				offX /= dist;
+				offY /= dist;
+				offX *= 3;
+				offY *= 3;
+				if (Math.random() < 0.05 + Draw.score / 5000d)
+					GameFrame.bullets.add(new Bullet(x, y, offX, offY));
+			}
+			//}
 			/*if (Math.random() < 0.5) {
 				// x
 				if (Math.random() < 0.5) {
@@ -109,93 +108,82 @@ public class GameFrame extends JFrame {
 						GameFrame.bullets.add(new Bullet((int) (Math.random() * GameFrame.player1.x), GameFrame.panelObject.getHeight(), Math.random() * 1, ((Math.random() + 0.5) * -1)));
 				}
 			}*/
-            for (Bullet b : GameFrame.bullets) b.update();
-            GameFrame.panelObject.repaint();
-        }
-    }
+			for (Bullet b : GameFrame.bullets) b.update();
+			GameFrame.panelObject.repaint();
+		}
+	}
 
 
-    public void getFullscreen() {
-        GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice gd = env.getDefaultScreenDevice();
+	public void getFullscreen() {
+		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice gd = env.getDefaultScreenDevice();
 
-        if (gd.isFullScreenSupported()) {
-            gd.setFullScreenWindow(window);
-        }
-    }
-
-
-    public void gameMenuScreen(Graphics2D g) {
-        int w = panelObject.getWidth() / 2;
-        int h = panelObject.getHeight() / 6;
-        int x = (getWidth() / 2) - w / 2;
-        int y = panelObject.getHeight() / 2;
-        g.drawImage(menuBackground, x, y, w, h / 2, this);
-        setVisible(true);
-        if (player1.clickedX != null && player1.clickedY != null && player1.clickedX > x && player1.clickedX < x + w) {
-            if (player1.clickedY > y && player1.clickedY < y + h / 2) {
-                player1.alive = true;
-                player1.clickedX = null;
-                player1.clickedY = null;
-                Draw.score = 0;
-                player1.x = GameFrame.panelObject.getWidth() / 2f;
-                player1.y = GameFrame.panelObject.getHeight() / 2f;
-            }
-        }
-    }
+		if (gd.isFullScreenSupported()) {
+			gd.setFullScreenWindow(window);
+		}
+	}
 
 
-    public void gameOverScreen(Graphics2D g) {
-        int wGameOver = window.getWidth() / 2;
-        int hGameOver = window.getHeight() / 6;
-        int xGameOver = (window.getWidth() / 2) - wGameOver / 2;
-        int yGameOver = window.getHeight() / 6;
+	public void gameMenuScreen(Graphics2D g) {
+		int w = panelObject.getWidth() / 2;
+		int h = panelObject.getHeight() / 6;
+		int x = (getWidth() / 2) - w / 2;
+		int y = panelObject.getHeight() / 2;
+		g.drawImage(menuBackground, x, y, w, h / 2, this);
+		setVisible(true);
+		if (player1.clickedX != null && player1.clickedY != null && player1.clickedX > x && player1.clickedX < x + w) {
+			if (player1.clickedY > y && player1.clickedY < y + h / 2) {
+				player1.alive = true;
+				player1.clickedX = null;
+				player1.clickedY = null;
+				Draw.score = 0;
+				player1.x = GameFrame.panelObject.getWidth() / 2f;
+				player1.y = GameFrame.panelObject.getHeight() / 2f;
+			}
+		}
+	}
 
-        int wRestart = window.getWidth() / 16;
-        int hRestart = window.getHeight() / 9;
-        int xRestart = xGameOver + wGameOver/ 2 - tryAgainIcon.getWidth() /2 ;
-        int yRestart = yGameOver + hRestart * 2;
-
-        int xMenu = xGameOver;
-        int wMenu = window.getWidth() / 16;
-        int hMenu = window.getHeight() / 9;
-        int yMenu = yGameOver + hMenu * 2;
-
-        int wExit = window.getWidth() / 16;
-        int hExit = window.getWidth() / 9;
-        int xExit = xMenu + wGameOver * 2 / 3;
-        int yExit = yGameOver + hMenu * 2;
-
-        setVisible(true);
-        g.drawImage(gameOverImage, xGameOver, yGameOver, wGameOver, hGameOver, this);
-        g.drawImage(menuIcon, xMenu, yMenu, wMenu, hMenu, this);
-        g.drawImage(tryAgainIcon, xRestart , yRestart, wRestart, hRestart, this);
-        g.drawImage(exitIcon, xExit, yExit, wExit, hExit, this);
-
-        if (player1.clickedX != null && player1.clickedY != null && player1.clickedX > xMenu + wGameOver * 2 / 3 && player1.clickedX < xMenu + wGameOver * 2 / 3 + wExit) {
-            if (player1.clickedY > yGameOver + hMenu * 2 && player1.clickedY < yGameOver + hMenu * 2 + hMenu) {
-                System.exit(0);
-            }
-        }
+	public boolean in(int x, int y, int w, int h, Integer mouseX, Integer mouseY) {
+		if (mouseX == null || mouseY == null) return false;
+		return mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h;
+	}
 
 
-        if (player1.clickedX != null && player1.clickedY != null && player1.clickedX > xMenu && player1.clickedX < xMenu + window.getWidth() / 16 ){
-            if (player1.clickedY != null && player1.clickedY != null && player1.clickedY > yGameOver + hMenu * 2 && player1.clickedY < yGameOver + hMenu * 2 + hMenu){
-                System.exit(0);
+	public void gameOverScreen(Graphics2D g) {
 
-            }
+		int wGameOver = window.getWidth() / 2;
+		int hGameOver = window.getHeight() / 6;
+		int xGameOver = (window.getWidth() / 2) - wGameOver / 2;
+		int yGameOver = window.getHeight() / 6;
 
-        }
-        if (player1.clickedX != null && player1.clickedY != null && player1.clickedX > xMenu + wGameOver / 3 && player1.clickedX < xMenu + wGameOver / 3 + wExit) {
-            if (player1.clickedY > yGameOver + hMenu * 2 && player1.clickedY < yGameOver + hMenu * 2 + hMenu) {
-                player1.alive = true;
-                player1.clickedX = null;
-                player1.clickedY = null;
-                Draw.score = 0;
-                player1.x = GameFrame.panelObject.getWidth() / 2f;
-                player1.y = GameFrame.panelObject.getHeight() / 2f;
-            }
-        }
+		int hIcon = window.getHeight() / 9;
+		int wIcon = window.getWidth() / 16;
+		int yIcon = window.getHeight() / 2 - window.getHeight() / 9;
+
+		int xRestart = window.getWidth() / 2 - wIcon / 2;
+
+		int xMenu = window.getWidth() / 4 - wIcon / 2;
+
+		int xExit = window.getWidth() - window.getWidth() / 4 - wIcon / 2;
+
+		setVisible(true);
+		g.drawImage(gameOverImage, xGameOver, yGameOver, wGameOver, hGameOver, this);
+		g.drawImage(menuIcon, xMenu, yIcon, wIcon, hIcon, this);
+		g.drawImage(tryAgainIcon, xRestart, yIcon, wIcon, hIcon, this);
+		g.drawImage(exitIcon, xExit, yIcon, wIcon, hIcon, this);
+
+		if (in(xMenu, yIcon, wIcon, hIcon, player1.clickedX, player1.clickedY)) {
+			System.exit(0);
+		}
+
+		if (in(xRestart, yIcon, wIcon, hIcon, player1.clickedX, player1.clickedY)) {
+			player1.alive = true;
+			player1.clickedX = null;
+			player1.clickedY = null;
+			Draw.score = 0;
+			player1.x = GameFrame.panelObject.getWidth() / 2f;
+			player1.y = GameFrame.panelObject.getHeight() / 2f;
+		}
       /*  if (player1.clickedX != null && player1.clickedY != null && player1.clickedX > x && player1.clickedX < x + w) {
             if (player1.clickedY > y && player1.clickedY < y + h / 2) {
                 player1.alive = true;
@@ -207,21 +195,21 @@ public class GameFrame extends JFrame {
             }
 
         }*/
-    }
+	}
 
-    public static class Update extends TimerTask {
-        @Override
-        public void run() {
-            GameFrame.player1.update();
-        }
-    }
+	public static class Update extends TimerTask {
+		@Override
+		public void run() {
+			GameFrame.player1.update();
+		}
+	}
 
-    public static class ScoreUpdate extends TimerTask {
-        @Override
-        public void run() {
-            if (player1.alive) {
-                Draw.score++;
-            }
-        }
-    }
+	public static class ScoreUpdate extends TimerTask {
+		@Override
+		public void run() {
+			if (player1.alive) {
+				Draw.score++;
+			}
+		}
+	}
 }
--- src/main/java/PlayerFigure.java
+++ src/main/java/PlayerFigure.java
@@ -1,52 +1,57 @@
 import java.awt.*;
 
 public class PlayerFigure {
-    public double x = GameFrame.window.getWidth() / 2f;
-    public double y = GameFrame.window.getHeight() / 2f;
-    public Integer clickedX;
-    public Integer clickedY;
-    private int width = 30;
-    private int height = 30;
-    public boolean alive = false;
+	public double x = GameFrame.window.getWidth() / 2f;
+	public double y = GameFrame.window.getHeight() / 2f;
+	public Integer clickedX;
+	public Integer clickedY;
+	private int width = 30;
+	private int height = 30;
+	public boolean alive = false;
 
+	public static double speed = 3.1;
 
-    public void draw(Graphics2D g) {
-        g.setColor(Color.CYAN);
-        g.fillOval((int) x - width / 2, (int) y - height / 2, width, height);
-        if (clickedX != null && clickedY != null)
-            g.drawOval(clickedX, clickedY, 1, 1);
-    }
+	public void draw(Graphics2D g) {
+		g.setColor(Color.CYAN);
+		g.fillOval((int) x - width / 2, (int) y - height / 2, width, height);
+		if (clickedX != null && clickedY != null)
+			g.drawOval(clickedX, clickedY, 1, 1);
+	}
 
-    public void update() {
-        for (int i = 0; i < GameFrame.bullets.size(); i++) {
-            double diffX = Math.abs(x - GameFrame.bullets.get(i).x);
-            double diffY = Math.abs(y - GameFrame.bullets.get(i).y);
-            double distance = Math.sqrt((diffX * diffX) + (diffY * diffY));
+	public void update() {
+		for (int i = 0; i < GameFrame.bullets.size(); i++) {
+			double diffX = Math.abs(x - GameFrame.bullets.get(i).x);
+			double diffY = Math.abs(y - GameFrame.bullets.get(i).y);
+			double distance = Math.sqrt((diffX * diffX) + (diffY * diffY));
 
-            if (distance < width / 2d + GameFrame.bullets.get(i).width / 2d) {
-                alive = false;
-                GameFrame.bullets.clear();
-                clickedX = null;
-                clickedY = null;
-            }
-        }
+			if (distance < width / 2d + GameFrame.bullets.get(i).width / 2d) {
+				alive = false;
+				GameFrame.bullets.clear();
+				clickedX = null;
+				clickedY = null;
+			}
+		}
 
-        if (clickedX != null && clickedY != null) {
-            double offX = clickedX - x;
-            double offY = clickedY - y;
+		if (clickedX != null && clickedY != null) {
+			double offX = clickedX - x;
+			double offY = clickedY - y;
 
-            if (Math.abs(offX) > 1 || Math.abs(offY) > 1) {
-                float distance = (float) Math.sqrt(offX * offX + offY * offY);
-                x += offX / distance;
-                y += offY / distance;
-            }
-        }
-    }
+			if (Math.abs(offX) > 1 || Math.abs(offY) > 1) {
+				float distance = (float) Math.sqrt(offX * offX + offY * offY);
+				double velX = offX / distance;
+				double velY = offY / distance;
+				velX *= speed;
+				velY *= speed;
+				x += velX;
+				y += velY;
+			}
+		}
+	}
 
-    public void saveClickedPoint(int x, int y) {
-        clickedX = x;
-        clickedY = y;
-    }
+	public void saveClickedPoint(int x, int y) {
+		clickedX = x;
+		clickedY = y;
+	}
 }
 
 
